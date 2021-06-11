2d972f39008e3a99c926c5a8014d35a0ccce4287
rahul rajput <rahulrajput7358@gmail.com>
2021-06-11 14:18:24 +0530

    almost  done

---
+++ Ride now/rideNow.js
@@ -0,0 +1,58 @@
+const arrOfBikes = [
+  {
+    name: "Bajaj CT 100",
+    free: "110",
+    excess: "2",
+    price: 79,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajCT100@2x.jpg",
+  },
+  {
+    name: "Honda Activa",
+    free: 80,
+    excess: 2,
+    price: 89,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/HondaActiva4G@2x.jpg",
+  },
+  {
+    name: "Honda Dio",
+    free: 67,
+    excess: 2,
+    price: 89,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajCT100@2x.jpg",
+  },
+  {
+    name: "Bajaj Pulsar 150",
+    free: 71,
+    excess: 2,
+    price: 139,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajCT100@2x.jpg",
+  },
+  {
+    name: "Bajaj Avenger 220 Street",
+    free: 293,
+    excess: 2.5,
+    price: 549,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajAvengerStreet220@2x.jpg",
+  },
+  {
+    name: "Bajaj Pulsar 150",
+    free: 71,
+    excess: 2,
+    price: 139,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajCT100@2x.jpg",
+  },
+  {
+    name: "Bajaj Pulsar 150",
+    free: 71,
+    excess: 2,
+    price: 139,
+    imgLink:
+      "https://d3bvfezcznypk7.cloudfront.net/BikeImages/JPG/BajajCT100@2x.jpg",
+  },
+];
